6ccacf7b4a46c0dd4d1d3c9feb0b554282f86267
liuwenjun <liuwenjun@hangsheng.com.cn>
2019-10-28 11:39:58 +0800

    fixed bug in build

--- build/gcc-imx6q.bat
+++ build/gcc-imx6q.bat
@@ -5,7 +5,7 @@ if exist 3rd_arm rd 3rd_arm /S/Q
 mkdir 3rd_arm
 cd 3rd_arm
 
-cmake ../../generate -G "%GENERATOR%" %TOOLCHAIN% -DCMAKE_INSTALL_PREFIX=../../stage -DPASSOA3RD=ON -DCMAKE_ARM_GCC=E:/usr/linaro/gcc -DCMAKE_MAKE_PROGRAM=E:/usr/linaro/gcc/bin/make.exe
+cmake ../../3rd/libjpeg-turbo -G "%GENERATOR%" %TOOLCHAIN% -DCMAKE_INSTALL_PREFIX=../../stage -DPASSOA3RD=ON -DCMAKE_ARM_GCC=E:/usr/linaro/gcc -DCMAKE_MAKE_PROGRAM=E:/usr/linaro/gcc/bin/make.exe
 cmake --build ./ --target install --config Release
 cd ..
 
